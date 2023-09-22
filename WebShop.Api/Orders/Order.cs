eb13557fcbc231b38c820e85c0cb151f9a911dbc
Fabio Galante Mans <fgamans@hotmail.com>
2023-09-22 19:36:50 -0300

    Initial commit

---
+++ WebShop.Api/Orders/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace WebShop.Api.Orders;
+
+public class Order
+{
+    public Guid Id { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public List<LineItem> LineItems { get; set; } = new();
+}
